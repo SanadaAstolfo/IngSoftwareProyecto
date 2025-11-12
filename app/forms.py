92a03b02bdbd3725118dd38c54fa905f25434ce4
Driukk <225207986+KaladinBPLT@users.noreply.github.com>
2025-11-11 23:16:19 -0300

    estado reproductivo

--- app/forms.py
+++ app/forms.py
@@ -6,7 +6,7 @@ from .models import Paciente, AtencionMedica, ChequeoFisico, Procedimiento, Docu
 class PacienteForm(forms.ModelForm):
     class Meta:
         model = Paciente
-        fields = ['foto', 'nombre', 'especie', 'raza', 'sexo', 'fecha_nacimiento', 'tutor', 'alertas']
+        fields = ['foto', 'nombre', 'especie', 'raza', 'sexo', 'Estado_Reproductivo', 'fecha_nacimiento', 'tutor', 'alertas']
         widgets = {
             'fecha_nacimiento': forms.DateInput(attrs={'type': 'date'}),
             'alertas': forms.CheckboxSelectMultiple,
